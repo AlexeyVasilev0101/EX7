fd0ccd52e361375d6eb773b0525a1a836ad65cf3
AlexeyVasilev0101 <101921598+AlexeyVasilev0101@users.noreply.github.com>
2022-05-06 15:11:27 +0300

    EX7

---
+++ EX7OOP/EX7OOP.cpp
@@ -0,0 +1,13 @@
+﻿#include "krug.h"
+#include "vector.h"
+#include "complex.h"
+#include <string>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	cout << "GOOD!";
+}
